6010b14c06ab1948c696e8e4a46d995d0fd728e6
Yammery80 <yammery.28u@gmail.com>
2024-07-10 12:55:42 -0600

    Seccion de Autores

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     return render_template('base.html')
 
 @app.route('/libros')
-def libro():
+def libros():
     #Conectar con DB(DataBase)
     conexion = psycopg2.connect(
         database="biblioteca",
@@ -24,10 +24,31 @@ def libro():
     # Crear un cursor (objeto para recorrer las tablas)
     cursor = conexion.cursor()
     #Ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM libro''')
+    cursor.execute('''SELECT * FROM libros_view''')
     #Recuperar información
     datos = cursor.fetchall()
-    #Cerrar cursor y conecxión a la base de datos
+    #Cerrar cursor y conexión a la base de datos
     cursor.close()
     conexion.close()
     return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+    #Conectar con DB(DataBase)
+    conexion = psycopg2.connect(
+        database="biblioteca",
+        user="postgres",
+        password="Lindel2005",
+        host="localhost",
+        port="5432"
+    )
+    # Crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    #Ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM autores_view''')
+    #Recuperar información
+    datos = cursor.fetchall()
+    #Cerrar cursor y conexión a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
