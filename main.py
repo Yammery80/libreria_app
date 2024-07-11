c4c0505616fb32ff6b3509d60cb790eb7b55aef9
Yammery80 <yammery.28u@gmail.com>
2024-07-11 14:05:12 -0600

    Países.html

--- main.py
+++ main.py
@@ -52,3 +52,24 @@ def autores():
     cursor.close()
     conexion.close()
     return render_template('autores.html', datos=datos)
+
+@app.route('/pais')
+def pais():
+    #Conectar con DB(DataBase)
+    conexion = psycopg2.connect(
+        database="biblioteca",
+        user="postgres",
+        password="Lindel2005",
+        host="localhost",
+        port="5432"
+    )
+    # Crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    #Ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM pais''')
+    #Recuperar información
+    datos = cursor.fetchall()
+    #Cerrar cursor y conexión a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('paises.html', datos=datos)
